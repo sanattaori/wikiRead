451aca5ccaa71d7f68b0f1eeaaea589d8bf65449
sanattaori <sanattaori3@gmail.com>
2018-06-12 13:35:30 +0530

    init

---
+++ app/src/main/java/sanattaori/speedev/com/wikiread/activities/SearchActivity.kt
@@ -0,0 +1,13 @@
+package sanattaori.speedev.com.wikiread.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import sanattaori.speedev.com.wikiread.R
+
+class SearchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+    }
+}
