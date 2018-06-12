cc42b7d36158f74e46dfc4c33856707d2704b738
sanattaori <sanattaori3@gmail.com>
2018-06-12 14:30:27 +0530

    Add Fragments

--- app/src/main/java/sanattaori/speedev/com/wikiread/activities/SearchActivity.kt
+++ app/src/main/java/sanattaori/speedev/com/wikiread/activities/SearchActivity.kt
@@ -1,7 +1,13 @@
 package sanattaori.speedev.com.wikiread.activities
 
+import android.app.SearchManager
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.SearchView
+import kotlinx.android.synthetic.main.activity_search.*
 import sanattaori.speedev.com.wikiread.R
 
 class SearchActivity : AppCompatActivity() {
@@ -9,5 +15,44 @@ class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        setSupportActionBar(toolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item!!.itemId == android.R.id.home){
+            finish()
+        }
+        return true
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.search_menu, menu)
+
+        val searchItem = menu!!.findItem(R.id.android_search)
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView = searchItem!!.actionView as SearchView
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        searchView.setIconifiedByDefault(false)
+        searchView.requestFocus()
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+
+                println("updated search")
+
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+                 //To change body of created functions use File | Settings | File Templates.
+            }
+        })
+
+        return super.onCreateOptionsMenu(menu)
     }
 }
---
+++ app/src/main/java/sanattaori/speedev/com/wikiread/fragments/HistoryFragment.kt
@@ -0,0 +1,30 @@
+package sanattaori.speedev.com.wikiread.fragments
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import sanattaori.speedev.com.wikiread.R
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class HistoryFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_history, container, false)
+    }
+
+
+}
